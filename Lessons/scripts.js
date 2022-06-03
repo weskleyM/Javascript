1a59e43e1c6c3125ddacd5ca813b06d1d8783507
weskleyM <weskley512@gmail.com>
2022-06-03 13:01:56 -0300

    scripts atualizado

--- Lessons/scripts.js
+++ Lessons/scripts.js
@@ -1234,3 +1234,39 @@ function move(event) {
             break;
     }
 }*/
+
+/*const myBtn = document.getElementById("myBtn");
+const myAnimation = document.getElementById("myDiv");
+
+myBtn.addEventListener("click", begin);
+
+function begin() {
+    let timerId = null;
+    // let x = 0;
+    // let y = 0;
+    // let degress = 0;
+    let scaleX = 1;
+    let scaleY = 1;
+
+    timerId = setInterval(frame, 5);
+
+    function frame() {
+        if (scaleX <= 2 && scaleY <= 2) {
+        // if (x <= 200 && y <= 200) {
+        // if (degress <= 360) {
+            scaleX += 0.01;
+            scaleY += 0.01
+            // x++;
+            // y++;
+            // degress+=3;
+            myAnimation.style.transform = "scale("+scaleX+", "+scaleY+")";
+            // myAnimation.style.top = y + "px";
+            // myAnimation.style.left = x + "px";            
+            // myAnimation.style.transform = "rotateX(" + degress + "deg)";
+            // myAnimation.style.transform = "rotateY(" + degress + "deg)";
+            // myAnimation.style.transform = "rotateZ(" + degress + "deg)";
+        } else {
+            clearInterval(timerId);
+        }
+    }
+}*/
