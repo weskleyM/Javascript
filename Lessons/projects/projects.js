2c2a406eb1488e261e4b316ff643d0107e25abcf
weskleyM <weskley512@gmail.com>
2022-06-14 15:54:13 -0300

    jokenpo game adicionado

--- Lessons/projects/projects.js
+++ Lessons/projects/projects.js
@@ -1,5 +1,6 @@
 // ** STOPWATCH SCRIPTS **
-const timerDisplay = document.querySelector("#timerDisplay");
+
+/*const timerDisplay = document.querySelector("#timerDisplay");
 const startBtn = document.querySelector("#startBtn");
 const pauseBtn = document.querySelector("#pauseBtn");
 const resetBtn = document.querySelector("#resetBtn");
@@ -36,7 +37,7 @@ resetBtn.addEventListener("click", () => {
     hrs = 0;
     mins = 0;
     secs = 0;
-    timerDisplay.textContent = "00:00:00"
+    timerDisplay.textContent = "00:00:00";
 });
 
 function updateTimer() {
@@ -53,6 +54,54 @@ function updateTimer() {
     timerDisplay.textContent = `${hrs}:${mins}:${secs}`;
 
     function pad(unit) {
-        return (("0") + unit).length > 2 ? unit : "0" + unit;
+        return ("0" + unit).length > 2 ? unit : "0" + unit;
+    }
+}*/
+
+// ** JO-KEN-PO GAME **
+
+/*const playerTxt = document.querySelector("#playerTxt");
+const pcTxt = document.querySelector("#pcTxt");
+const resultTxt = document.querySelector("#resultTxt");
+const choiceBtn = document.querySelectorAll(".gameBtn");
+let player;
+let pc;
+let result;
+
+choiceBtn.forEach((btn) =>
+    btn.addEventListener("click", () => {
+        player = btn.textContent;
+        pcTurn();
+        playerTxt.textContent = `Jogador: ${player}`;
+        pcTxt.textContent = `Computador: ${pc}`;
+        resultTxt.textContent = `Resultado: ${checkWinner()}`;
+    })
+);
+
+function pcTurn() {
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch (randNum) {
+        case 1:
+            pc = "Pedra";
+            break;
+        case 2:
+            pc = "Papel";
+            break;
+        case 3:
+            pc = "Tesoura";
+            break;
     }
 }
+
+function checkWinner() {
+    if (player == pc) {
+        return "Empate!";
+    } else if (pc == "Pedra") {
+        return player == "Papel" ? "Você Ganhou!" : "Você Perdeu!";
+    } else if (pc == "Papel") {
+        return player == "Tesoura" ? "Você Ganhou!" : "Você Perdeu!";
+    } else if (pc == "Tesoura") {
+        return player == "Pedra" ? "Você Ganhou!" : "Você Perdeu!";
+    }
+}*/
